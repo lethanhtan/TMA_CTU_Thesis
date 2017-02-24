bee1f2a4b4053b846959fad2c840c722b43a4042
Trần Lê Quế Ngọc <banhmatongnhantao@gmail.com>
2017-02-24 22:48:26 +0700

    Update CreateTourController.java

--- code/src/main/java/edu/ctu/thesis/travelsystem/controller/CreateTourController.java
+++ code/src/main/java/edu/ctu/thesis/travelsystem/controller/CreateTourController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("createtour")
-public class CreateTour {
+public class CreateTourController {
  
    @RequestMapping(method = RequestMethod.GET)
-   public String aboutController() {
+   public String createtourController() {
 
       return "createtour";
    }
