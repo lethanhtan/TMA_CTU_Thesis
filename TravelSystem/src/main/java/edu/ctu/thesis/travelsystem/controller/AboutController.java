8351a467b5cfc48a8793a48e75a2d2065bddf142
B1304751 <yb1304751@student.ctu.edu.vn>
2017-03-16 09:53:09 -0700

    Remove something!

--- TravelSystem/src/main/java/edu/ctu/thesis/travelsystem/controller/AboutController.java
+++ TravelSystem/src/main/java/edu/ctu/thesis/travelsystem/controller/AboutController.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/aboutus")
 public class AboutController{
 	
-   @RequestMapping(method = RequestMethod.GET)
+   @RequestMapping(value = "aboutus", method = RequestMethod.GET)
    public String aboutController() {
 
       return "aboutus";
