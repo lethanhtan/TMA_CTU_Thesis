febc7167ae60446045ac1469470b39219702c119
B1304751 <yb1304751@student.ctu.edu.vn>
2017-03-16 10:08:44 -0700

    Remove unused!

--- TravelSystem/src/main/java/edu/ctu/thesis/travelsystem/dao/ImportDataDao.java
+++
@@ -1,7 +0,0 @@
-package edu.ctu.thesis.travelsystem.dao;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface ImportDataDao {
-	public void importExcel(MultipartFile excelfile);
-}
--- TravelSystem/src/main/java/edu/ctu/thesis/travelsystem/service/ImportDataService.java
+++
@@ -1,7 +0,0 @@
-package edu.ctu.thesis.travelsystem.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface ImportDataService {
-	public void importExcel(MultipartFile excelfile);
-}
