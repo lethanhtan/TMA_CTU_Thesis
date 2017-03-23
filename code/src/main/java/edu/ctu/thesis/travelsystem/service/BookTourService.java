6e7468781f7eb19d08fe9e5408cd0d6e175ade66
cayoccho <banhmatongnhantao@gmail.com>
2017-03-23 20:21:38 +0700

    Fix some bugs

--- code/src/main/java/edu/ctu/thesis/travelsystem/service/BookTourService.java
+++ code/src/main/java/edu/ctu/thesis/travelsystem/service/BookTourService.java
@@ -3,38 +3,31 @@ package edu.ctu.thesis.travelsystem.service;
 import java.util.List;
 
 import edu.ctu.thesis.travelsystem.model.BookTour;
-import edu.ctu.thesis.travelsystem.model.RegistrationInfo;
 
 public interface BookTourService {
 	public void saveBookTour(BookTour bookTour, int idTour);
 	
 	public List<BookTour> listBookTour();
-	
-	public List<BookTour> registrationList(int idTour);
-	
+
 	public BookTour searchById(int idBT);
 	
 	public void editBookTour(BookTour bookTour);
 
-	public void deleteBookTour(int idBT, int idTour);
-	
 	public BookTour searchByName(String cusName);
 	
 	public BookTour searchByEmail(String cusEmail);
 	
 	public BookTour searchByPhone(String cusPhone);
 	
-	public Integer getNumBookTour(int idTour);
+	public List<BookTour> registrationListByValue(String value, int idTour);
 	
-	public List<BookTour> registrationListByValue(String value);
-	
-	public Integer getNumBTBySearch(String value);
-	
-	public Integer paginationX(Integer currentPage, Integer page);
-	
-	public Integer paginationY(Integer numOfPage, Integer currentPage, Integer page);
+	public Integer getNumBTBySearch(String value, int idTour);
 	
 	public int getNumTicketBooked(int idTour);
 	
 	public int getNumTicketAvailability(int idTour);
+	
+	public List<BookTour> registrationInfoByValue(String value, int idTour);
+	
+	public void cancelBookTour(int idBT, int idTour);
 }
