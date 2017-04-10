cca84ec7c5a00033f75fb5009c501e2982b0892a
cayoccho <banhmatongnhantao@gmail.com>
2017-04-10 21:37:53 +0700

    Delete some files useless

--- code/src/main/webapp/resources/js/displayinfo.js
+++
@@ -1,6 +0,0 @@
-/* Back To Top */
-document.getElementById('test').addEventListener('change', function () {
-    var style = this.value == 1 ? 'block' : 'none';
-    document.getElementById('hidden_div').style.display = style;
-});
-/* End Back To Top */
