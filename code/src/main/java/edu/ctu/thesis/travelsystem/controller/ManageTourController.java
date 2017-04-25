ef4dc7ea67906b08b6b8aad9c071ee14cbba6858
B1304751 <yb1304751@student.ctu.edu.vn>
2017-04-25 14:43:52 +0700

    Merge fix for confilict

--- code/src/main/java/edu/ctu/thesis/travelsystem/controller/ManageTourController.java
+++ code/src/main/java/edu/ctu/thesis/travelsystem/controller/ManageTourController.java
@@ -27,7 +27,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 
 import edu.ctu.thesis.travelsystem.extra.Pagination;
 import edu.ctu.thesis.travelsystem.model.BookTour;
